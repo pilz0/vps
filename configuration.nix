4e9c3c414accbc0e73e57cc442fb38100a35a742
pilz0 <marie0@riseup.net>
2024-02-20 17:15:49 +0100

    maybe it was the autoupdate

--- configuration.nix
+++ configuration.nix
@@ -148,17 +148,17 @@
 # spotifyd is a service for streaming spotify to this device
 services.spotifyd.enable = true;
 # Autoupdate
-system.autoUpgrade = {
-  enable = true;
-  flake = inputs.self.outPath;
-  flags = [
-    "--update-input"
-    "nixpkgs"
-    "-L" # print build logs
-  ];
-  dates = "02:00";
-  randomizedDelaySec = "45min";
-};
+#system.autoUpgrade = {
+  #enable = true;
+  #flake = inputs.self.outPath;
+  #flags = [
+    #"--update-input"
+    #"nixpkgs"
+   # "-L" # print build logs
+  #];
+  #dates = "02:00";
+ # randomizedDelaySec = "45min";
+#};
 
 
 # Openssh
