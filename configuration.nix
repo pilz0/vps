f5ff134f94ef1ea3aced286ba4017df6a073314f
pilz0 <marie0@riseup.net>
2024-03-22 11:43:15 +0100

    fixed smt

--- configuration.nix
+++ configuration.nix
@@ -1,3 +1,4 @@
+
 ### Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
@@ -14,13 +15,10 @@
   boot.loader.efi.canTouchEfiVariables = true;
   #Hostname
   networking.hostName = "serva";
-
   #Self doxx UwU
   time.timeZone = "Europe/Berlin";
-
   # Select internationalisation properties.
   i18n.defaultLocale = "de_DE.UTF-8";
-
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "de_DE.UTF-8";
     LC_IDENTIFICATION = "de_DE.UTF-8";
@@ -32,7 +30,6 @@
     LC_TELEPHONE = "de_DE.UTF-8";
     LC_TIME = "de_DE.UTF-8";
   };
-
   console.keyMap = "de";
   # Enable sound with pipewire.
   sound.enable = true;
@@ -44,12 +41,10 @@
     alsa.support32Bit = true;
     pulse.enable = true;
   };
-
   #my user account
       users.users.marie.isNormalUser = true;
       users.users.marie.description = "marie";
       users.users.marie.extraGroups = [ "networkmanager" "wheel" ];
-
     
   #Services
   #zsh
@@ -65,10 +60,8 @@
      };
   # Maii keyy :3 
   users.users.marie.openssh.authorizedKeys.keys = ["sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIBTGgUYUsIAtcbZBqk5Mq0LH2T5KGFjdjAgNIwUf+/LBAAAABHNzaDo= pilz@framewok"];
-
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
-
   #Docker
   virtualisation.docker.enable = true;
   #All my Programms :3
@@ -110,19 +103,15 @@
     picocom
     dnsmasq
     spotifyd
-    arion
     pipes
     ];
-
     #an openssh banner, is shown everytime you try to connect
     services.openssh.banner = "
       ***************************************************************************
                                   NOTICE TO USERS
-
       This is a Federal computer system and is the property of the United
       States Government. It is for authorized use only. Users (authorized or
       unauthorized) have no explicit or implicit expectation of privacy.
-
       Any or all uses of this system and all files on this system may be
       intercepted, monitored, recorded, copied, audited, inspected, and disclosed to
       authorized site, Department of Energy, and law enforcement personnel,
@@ -130,19 +119,14 @@
       By using this system, the user consents to such interception, monitoring,
       recording, copying, auditing, inspection, and disclosure at the discretion of
       authorized site or Department of Energy personnel.
-
       Unauthorized or improper use of this system may result in administrative
       disciplinary action and civil and criminal penalties. By continuing to use
       this system you indicate your awareness of and consent to these terms and
       conditions of use. LOG OFF IMMEDIATELY if you do not agree to the conditions
       stated in this warning.
-
       *****************************************************************************";
-
-
 # spotifyd is a service for streaming spotify to this device
 services.spotifyd.enable = true;
-
   programs.zsh.enable = true;
     programs.zsh.ohMyZsh.enable = true;
     programs.zsh.ohMyZsh.theme = "crunch";
@@ -168,7 +152,7 @@ virtualisation.arion = {
     service.ports = [ "3000:3001" ];
     service.restart = "unless-stopped";
   };
-
+};
 # Autoupdate 
 system.autoUpgrade = {
   enable = true;
@@ -181,8 +165,6 @@ system.autoUpgrade = {
   dates = "02:00";
   randomizedDelaySec = "45min";
 };
-
-
   environment.sessionVariables.NIXPKGS_ALLOW_UNFREE="1"; 
 # Openssh
   services.openssh.enable = true;
@@ -195,7 +177,6 @@ system.autoUpgrade = {
   # networking.firewall.enable = false;
 # NixOS Version
   system.stateVersion = "23.11"; # Did you read the comment?
-
 }
   ## github copilot wrote this
 #  I hope you can help me. 
