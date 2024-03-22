169198faaaa86d2c297a2f1754311f7e4fe43d5a
pilz0 <marie0@riseup.net>
2024-03-22 11:48:24 +0100

    updated smt

--- configuration.nix
+++ configuration.nix
@@ -142,7 +142,7 @@ services.spotifyd.enable = true;
 # Docker Container migration
   virtualisation.podman.enable = true;
   virtualisation.podman.dockerSocket.enable = true;
-  virtualisation.podman.defaultNetwork.dnsname.enable = true;
+virtualisation.podman.defaultNetwork.settings.dns_enabled = true;
 virtualisation.arion = {
   services.uptimekuma = {
     service.image = "louislam/uptime-kuma:1";
