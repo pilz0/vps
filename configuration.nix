dfb0e6234eb419a9bda44535ac82fa182f06e55f
pilz0 <marie0@riseup.net>
2024-04-17 19:36:18 +0200

    :3

--- configuration.nix
+++ configuration.nix
@@ -190,8 +190,8 @@ systemd.services. "rebuild" = {
 };
 
       # Open ports in the firewall.
-   networking.firewall.allowedTCPPorts = [ 1100 11000 81 8080 443 80 22 3000 8443 1337 ];
-   networking.firewall.allowedUDPPorts = [ 1100 11000 81 8080 443 80 22 3000 8443 1337 ];
+   networking.firewall.allowedTCPPorts = [ 1100 11000 81 8080 443 80 22 3000 8443 1337 3001 ];
+   networking.firewall.allowedUDPPorts = [ 1100 11000 81 8080 443 80 22 3000 8443 1337 3001 ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 # NixOS Version
